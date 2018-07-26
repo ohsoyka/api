aa36c47188fdffb5b210cf0fe73d2943f7c43f08
poohitan <poohitan@gmail.com>
2018-07-26 15:41:10 +0300

    Search: take care of search queries typed in wrong keyboard layout

--- routes/search.js
+++ routes/search.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const ConvertLayout = require('convert-layout');
 const models = require('../models');
 const errorHandler = require('../middlewares/error-handler');
+const detectAlphabet = require('../helpers/detect-alphabet');
 const generateQueryPopulations = require('../helpers/generate-query-populations');
 
 const router = express.Router();
@@ -22,7 +24,44 @@ function searchText(model, query, additionalFields = {}, populations = {}) {
     });
 }
 
-router.get('/', (req, res, next) => {
+async function performSearchForAllModels(query, filter, populations) {
+  const [articles, pages, projects, categories, photoAlbums] = await Promise.all([
+    searchText(models.article, query, filter, populations),
+    searchText(models.page, query, filter, populations),
+    searchText(models.project, query, filter, populations),
+    searchText(models.category, query, filter, populations),
+    searchText(models.photoAlbum, query, filter, populations),
+  ]);
+
+  const articlesSearchResults = articles.map(article => Object.assign({ searchResultType: 'article' }, article.serialize()));
+  const pagesSearchResults = pages.map(page => Object.assign({ searchResultType: 'page' }, page.serialize()));
+  const projectsSearchResults = projects.map(project => Object.assign({ searchResultType: 'project' }, project.serialize()));
+  const categoriesSearchResults = categories.map(category => Object.assign({ searchResultType: 'category' }, category.serialize()));
+  const photoAlbumsSearchResults = photoAlbums.map(photoAlbum => Object.assign({ searchResultType: 'photo-album' }, photoAlbum.serialize()));
+
+  const searchResults = [
+    ...articlesSearchResults,
+    ...pagesSearchResults,
+    ...projectsSearchResults,
+    ...categoriesSearchResults,
+    ...photoAlbumsSearchResults,
+  ]
+    .sort((left, right) => {
+      if (left.score > right.score) {
+        return -1;
+      }
+
+      if (left.score < right.score) {
+        return 1;
+      }
+
+      return 0;
+    });
+
+  return searchResults;
+}
+
+router.get('/', async (req, res, next) => {
   const { query, page = 1, limit = Number.MAX_SAFE_INTEGER } = req.query;
 
   if (query === null || query === undefined) {
@@ -38,44 +77,30 @@ router.get('/', (req, res, next) => {
     filter.private = false;
   }
 
-  Promise.all([
-    searchText(models.article, query, filter, populations),
-    searchText(models.page, query, filter, populations),
-    searchText(models.project, query, filter, populations),
-    searchText(models.category, query, filter, populations),
-  ])
-    .then(([articles, pages, projects, categories]) => {
-      const articlesSearchResults = articles.map(article => Object.assign({ searchResultType: 'article' }, article.serialize()));
-      const pagesSearchResults = pages.map(page => Object.assign({ searchResultType: 'page' }, page.serialize())); // eslint-disable-line
-      const projectsSearchResults = projects.map(project => Object.assign({ searchResultType: 'project' }, project.serialize()));
-      const categoriesSearchResults = categories.map(category => Object.assign({ searchResultType: 'category' }, category.serialize()));
+  const queryLanguage = detectAlphabet(query);
+  let queryInOppositeKeyboardLayout;
 
-      const searchResults = [
-        ...articlesSearchResults,
-        ...pagesSearchResults,
-        ...projectsSearchResults,
-        ...categoriesSearchResults,
-      ]
-        .sort((left, right) => {
-          if (left.score > right.score) {
-            return -1;
-          }
+  if (queryLanguage) {
+    queryInOppositeKeyboardLayout = queryLanguage === 'eng' ? ConvertLayout.uk.fromEn(query) : ConvertLayout.uk.toEn(query);
+  }
 
-          if (left.score < right.score) {
-            return 1;
-          }
+  try {
+    const searchResults = await performSearchForAllModels(query, filter, populations);
+    const searchResultsInOppositeKeyboardLayout = queryInOppositeKeyboardLayout
+      ? await performSearchForAllModels(queryInOppositeKeyboardLayout, filter, populations)
+      : [];
 
-          return 0;
-        });
+    const allSearchResults = searchResults.concat(searchResultsInOppositeKeyboardLayout);
 
-      const totalPages = Math.ceil(searchResults.length / limit);
+    const totalPages = Math.ceil(allSearchResults.length / limit);
 
-      res.json({
-        docs: searchResults.slice((page - 1) * limit, page * limit),
-        meta: { currentPage: page, totalPages },
-      });
-    })
-    .catch(next);
+    res.json({
+      docs: allSearchResults.slice((page - 1) * limit, page * limit),
+      meta: { currentPage: page, totalPages },
+    });
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.use(errorHandler);
